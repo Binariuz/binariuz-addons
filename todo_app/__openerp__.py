5ca6baedbceca31847d9a916a58626c0a9aa68c1
Milton Reis <milton@binariuz.com.br>
2016-08-30 13:37:59 -0400

    Commit inicial livro Daniel Reis

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'To-Do Aplicacao',
+    'description': 'Gerenciar suas Tarefas pessoais com este modulo',
+    'version': '8.0',
+    'author': 'Milton Reis',
+    'license': 'AGPL-3',
+    'website': 'http://www.binariuz.com.br',
+    'depends':['mail'],
+    'applications':True,
+    'contributors': ['Milton Reis <milton@binariuz.com.br>'],
+}
