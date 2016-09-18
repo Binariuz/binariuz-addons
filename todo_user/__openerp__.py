15b51cd69ff854a1857a0f4d3d10e3d6df68c85f
Milton Reis <milton@binariuz.com.br>
2016-09-18 07:57:05 -0400

    Chapter 3. Inheritance - Extending Existing Applications

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -3,4 +3,9 @@
     'description': 'Extend the To-Do app to multuser.',
     'author': 'Milton Reis',
     'depends': ['todo_app'],
+    'data': [
+        'views/todo_view.xml',
+        'security/todo_access_rules.xml',
+    ],
+
 }
